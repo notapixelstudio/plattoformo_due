bd36cb6e75867d776da962d34df8d433e32fe126
nitaku <matteo.abrate@gmail.com>
2023-11-10 15:15:47 +0100

    statechart platformer with coyote time and jump buffer

--- actors/Hero.gd
+++ actors/Hero.gd
@@ -9,17 +9,24 @@ const JUMP_VELOCITY = 400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	InputBuffer.add_monitored_action('ui_accept')
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = -jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
+	$StateChart.set_expression_property('velocity', velocity.y)
+	
+	if is_on_floor():
+		$StateChart.send_event('on_floor')
+	else:
+		$StateChart.send_event('airborne')
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		$StateChart.send_event('start_jump')
+		
+	if Input.is_action_just_released("ui_accept"):
+		$StateChart.send_event('end_jump')
+		
+	
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
@@ -28,3 +35,31 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	
+	
+func _apply_gravity(delta):
+	velocity.y += gravity * delta
+	
+func _apply_strong_gravity(delta):
+	velocity.y += 2 * gravity * delta
+	
+	
+func _on_jumping_state_entered():
+	velocity.y = -jump_velocity
+
+func _on_jumping_state_physics_processing(delta):
+	_apply_gravity(delta)
+	if velocity.y > 0:
+		$StateChart.send_event('end_jump')
+
+func _on_falling_state_physics_processing(delta):
+	_apply_strong_gravity(delta)
+
+func _on_coyote_time_state_physics_processing(delta):
+	_apply_strong_gravity(delta)
+
+func _on_landing_state_entered():
+	var is_jump_action_buffered = InputBuffer.is_action_buffered('ui_accept', 200)
+	if is_jump_action_buffered:
+		print('Executing buffered jump')
+		$StateChart.send_event('start_jump')
---
+++ global/InputBuffer.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var _monitor := {}
+
+func _input(event):
+	_record_pressed(event)
+	_delete_released.call_deferred(event) # let other _input handlers do their job before deletion
+	
+func _record_pressed(event):
+	for action in _monitor:
+		if event.is_action_pressed(action):
+			_monitor[action] = Time.get_ticks_msec()
+	
+func _delete_released(event):
+	for action in _monitor:
+		if event.is_action_released(action):
+			_monitor[action] = null
+
+func add_monitored_action(action : String):
+	_monitor[action] = null # no timestamp yet
+
+func is_action_buffered(action : String, duration_msec : int):
+	return action in _monitor and _monitor[action] != null and Time.get_ticks_msec() - _monitor[action] <= duration_msec
